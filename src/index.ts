4bb471cbaea3278de62c8be24571827496323c19
Pramendra Gupta <pramendra@gmail.com>
2020-06-15 11:07:29 +0900

    setup prismic webhook

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,16 @@ import express from 'express';
 const routeMain = express.Router({});
 
 routeMain.get('/', function (req: any, res: any) {
-  const { GCP_REGION } = process.env;
-  console.log('>>>', GCP_REGION);
   res.status(200).send('Hello World!');
 });
 
 routeMain.post('/webhook', (req: any, res: any) => {
-  // const secret = res.body;
-  // const { PRISMIC_WEBHOOK_SECRET, PRISMIC_API, TEST } = process.env;
-  // console.log(
-  //   // res.body,
-  //   TEST,
-  //   secret !== PRISMIC_WEBHOOK_SECRET,
-  //   secret,
-  //   PRISMIC_API,
-  //   PRISMIC_WEBHOOK_SECRET
-  // );
-  // if (secret !== PRISMIC_WEBHOOK_SECRET) res.status(400).send('webhook');
-  // else res.status(200).send('webhook');
+  const { PRISMIC_WEBHOOK_SECRET } = process.env;
+  const { secret = '' } = res.body || {};
+  if (secret && secret !== PRISMIC_WEBHOOK_SECRET) {
+    res.status(403).send('permission denied');
+    return;
+  }
   res.status(200).send('webhook');
 });
 
