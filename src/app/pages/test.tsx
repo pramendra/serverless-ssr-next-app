c266c116b80b042bee1b45752cf4d738ed658e29
Pramendra Gupta <pramendra@gmail.com>
2020-06-30 20:58:39 +0900

    fixed eslint

--- src/app/pages/test.tsx
+++ src/app/pages/test.tsx
@@ -4,7 +4,7 @@ import { NextPage } from 'next';
 const Page: NextPage = () => {
   const env = process.env.NODE_ENV;
 
-  return <p>Hello World! We\'re in ${env}!</p>;
+  return <p>Hello World! We are in {env}!</p>;
 };
 
 export default Page;
